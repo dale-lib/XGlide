59891ed2476d57b1bb3d89ff7418965e3f3a682a
div <441623442@qq.com>
2021-10-13 23:59:17 +0800

    first commit

---
+++ app/src/main/java/com/x/dale/libapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.x.dale.libapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dale.glide.GlideApp;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        GlideApp
+    }
+}
